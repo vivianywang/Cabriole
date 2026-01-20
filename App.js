efbd0b675cde28563636a02a232ef78298a2a120
vivianwang <vvccwang@gmail.com>
2026-01-20 13:50:02 -0500

    Add camera and photo permissions to app.json

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hi fiona</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
